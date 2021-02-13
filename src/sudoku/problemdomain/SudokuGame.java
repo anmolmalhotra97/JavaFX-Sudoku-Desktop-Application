9ee11ce98e3c4691fb6b776f3491cb4ac85c6ad7
Anmol Malhotra <anmol.malhotra@cleartrip.com>
2021-02-14 02:02:29 +0530

    basic foundation for problem statement

---
+++ src/sudoku/problemdomain/SudokuGame.java
@@ -0,0 +1,25 @@
+package sudoku.problemdomain;
+
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+
+    private final GameState gameState;
+    private final int[][] gridState;
+
+    public static final int GRID_BOUNDARY = 9;
+
+    public SudokuGame(GameState gameState, int[][] gridState) {
+        this.gameState = gameState;
+        this.gridState = gridState;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public int[][] getCopyOfGridState() {
+        // in order to make this sudoku grid state to be immutable so that sudoku object isn't messed with over time
+        return SudokuUtilities.copyToNewArray(gridState);
+    }
+}
